d24c2457341780b5c8ee1926af8f4640ae8252bb
devopsboy666 <pakawatpenpoom@gmail.com>
2025-04-24 10:02:54 +0700

    test Exam 1

--- src/com/logic/Example1UniqueChar.groovy
+++ src/com/logic/Example1UniqueChar.groovy
@@ -1,2 +1,15 @@
 package com.logic
 
+class Example1UniqueChar implements Serializable {
+    def steps
+    def message 
+
+    Example1UniqueChar(Map args) {
+        this.steps = args.steps
+        this.message = args.message
+    }
+
+    showString() {
+        return this.message[0]
+    }
+}
