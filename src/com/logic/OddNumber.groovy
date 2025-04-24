c37d98e63cd0687419d8fee2461b07992d193f6e
devopsboy666 <pakawatpenpoom@gmail.com>
2025-04-24 13:59:07 +0700

    Implement OddNumber Class

---
+++ src/com/logic/OddNumber.groovy
@@ -0,0 +1,26 @@
+package com.logic
+
+class OddNumber implements Serializable {
+
+    def steps
+    def number
+    def str_number
+
+    OddNumber(Map args) {
+        this.steps = args.steps ?: this
+        this.str_number = args.number
+        this.number = str_number.toInteger() ?: -1 
+
+        if( this.number < 3) {
+            error("Number much > 3")
+        } 
+    }
+
+    def isOddNumber() {
+        if( this.number % 2 == 1) {
+            steps.sh "echo Number ${this.number} is Odd Number"
+        } else {
+            error("Number much be Odd Number")
+        }
+    }
+}
