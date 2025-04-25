1f0b35278090db1993e31c0c8b39f5a518aa829d
devopsboy666 <pakawatpenpoom@gmail.com>
2025-04-25 18:17:43 +0700

    Implement Class Word count & pipeline

---
+++ src/com/logic/ExampleWordCounter.groovy
@@ -0,0 +1,44 @@
+package com.logic
+
+class ExampleWordCounter implements Serializable {
+
+    def steps 
+    def message
+
+    ExampleWordCounter(Map args) {
+        this.steps = args.steps
+        this.message = args.message 
+    }
+
+    def countDuplicateWords() {
+
+        def arr_mess = this.message.split(" ")
+        def length = arr_mess.size()
+        def arr_count = []
+        def count
+        def map_count = [:]
+        def newLength
+
+        for (int i=0; i<length; i++) {
+            count = 0
+            for (int j=i+1; j<length; j++) {
+                if (arr_mess[i] == args[j]) {
+                    count = count + 1
+                }
+            }
+            map_count["word"] = arr_mess[i]
+            map_count["count"] = count
+            arr_count << map_count
+        } 
+
+        newLength = arr_count.size()
+        steps.sh "touch record.txt"
+        
+        for (int i=0; i<newLength; i++) {
+            steps.sh "echo ${arr_count[i].word} = ${arr_count[i].count} >> record.txt" 
+        }
+
+        steps.sh "cat record.txt"
+    }
+
+}
