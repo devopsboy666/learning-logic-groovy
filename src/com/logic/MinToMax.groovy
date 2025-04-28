90cd0cfef565de8c4aeb821add6872715d98b7bf
devopsboy666 <pakawatpenpoom@gmail.com>
2025-04-28 14:40:59 +0700

    Implement MinToMax Class & Pipeline

---
+++ src/com/logic/MinToMax.groovy
@@ -0,0 +1,26 @@
+package com.logic
+
+class MinToMax implements Serializable {
+
+    def steps
+    def numbers
+
+    MinToMax(Map args) {
+        this.steps = args.steps
+        this.numbers = args.numbers
+    }
+
+    def sortNumbers() {
+        def tmp
+        def length = this.numbers.size()
+        for (int i=0; i<length; i++) {
+            for (int j=i; j<0; j--) {
+                if (this.numbers[j] < this.numbers[j-1]) {
+                    tmp = this.numbers[j-1]
+                    this.numbers[j-1] = this.numbers[j]
+                    this.numbers[j] = tmp
+                }
+            }
+        }
+    }
+}
